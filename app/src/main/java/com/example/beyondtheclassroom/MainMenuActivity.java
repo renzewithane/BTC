fdf64730c4b6b95d1865984ae842af39dd66d772
Christine Toral <116357988+chrstntrl@users.noreply.github.com>
2024-06-27 17:39:31 +0800

    Added MainMenu fragments

--- app/src/main/java/com/example/beyondtheclassroom/MainMenuActivity.java
+++ app/src/main/java/com/example/beyondtheclassroom/MainMenuActivity.java
@@ -6,6 +6,9 @@ import android.view.View;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.beyondtheclassroom.mainmenu.dev_navigation;
+import com.example.beyondtheclassroom.mainmenu.splash_screen;
+
 public class MainMenuActivity extends AppCompatActivity {
 
     @Override
@@ -13,49 +16,13 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainmenu);
 
-        Button buttonOperations = findViewById(R.id.button_operations);
-        buttonOperations.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainMenuActivity.this, OperationsActivity.class);
-                startActivity(intent);
-            }
-        });
+        if (savedInstanceState == null) {
+            splash_screen SplashScreen = new splash_screen();
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.splash_screen_container, SplashScreen)
+                    .commit();
+        }
 
-        Button buttonLevelBoard = findViewById(R.id.button_level_board);
-        buttonLevelBoard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainMenuActivity.this, LevelBoardActivity.class);
-                startActivity(intent);
-            }
-        });
 
-        Button buttonUserAvatar = findViewById(R.id.button_user_avatar);
-        buttonUserAvatar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainMenuActivity.this, UserAvatarActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button buttonStories = findViewById(R.id.button_stories);
-        buttonStories.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainMenuActivity.this, StoriesActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        Button buttonAssessments = findViewById(R.id.button_assessments);
-        buttonAssessments.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainMenuActivity.this, AssessmentsActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
