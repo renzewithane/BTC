00954cbd5cbe7b2de2c208c45514a1c50b9d2651
Renze Meinard Mortiga <98935669+renzewithane@users.noreply.github.com>
2024-06-29 00:03:24 +0800

    Added Levelboard Background

--- app/src/main/java/com/example/beyondtheclassroom/AssessmentsActivity.java
+++ app/src/main/java/com/example/beyondtheclassroom/AssessmentsActivity.java
@@ -8,21 +8,4 @@ import androidx.navigation.ui.NavigationUI;
 
 public class AssessmentsActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_assessments);
-
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_container_assessments);
-        NavController navController = navHostFragment.getNavController();
-        NavigationUI.setupActionBarWithNavController(this, navController);
-    }
-
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = ((NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.fragment_container_assessments)).getNavController();
-        return navController.navigateUp() || super.onSupportNavigateUp();
-    }
 }
